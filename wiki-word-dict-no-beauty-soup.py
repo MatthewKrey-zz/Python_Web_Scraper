bdc1f24df9034be048bcaa59fe7f41b619213389
MatthewKrey <kreyenglish@gmail.com>
2015-10-21 13:56:45 -0400

    add comments

--- wiki-word-dict-no-beauty-soup.py
+++ wiki-word-dict-no-beauty-soup.py
@@ -1,20 +1,11 @@
-# This experiment will explore scraping the words
-#from a Wikipedia article's content section 
+# This experiment will explore scraping the words from a Wikipedia article's
+# content section without BeautifulSoup & using Regular Expressions
 
 # make this a WikiScraper class for scaling to scrape other wiki pages
 
-from bs4 import BeautifulSoup
-from urllib import urlopen
-
-BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
-
-def make_soup(BASE_URL):
-    html = urlopen(BASE_URL).read()
-    return BeautifulSoup(html, "lxml")
-
-def get_body_content(BASE_URL):
-    soup = make_soup(BASE_URL)
-    body_content = soup.find("div", {"id": "bodyContent"})
-    return body_content
-
-get_body_content(BASE_URL)
+import urllib2
+from urllib2 import urlopen
+import re
+import cookielib, urllib2           # if site requires us to store cookies 
+from cookielib import CookieJar
+import datetime
