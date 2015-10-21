40258c15a5058d4b6068aeb31aef0560047fd712
MatthewKrey <kreyenglish@gmail.com>
2015-10-21 12:58:23 -0400

    return body content success

--- wiki-word-dict.py
+++ wiki-word-dict.py
@@ -2,19 +2,20 @@
 # BeautifulSoup & lxml libraries for HTML/XML Parsing
 # urllib for making HTTP Requests
 
+# make this a WikiScraper class for scaling to scrape other wiki pages
+
 from bs4 import BeautifulSoup
 from urllib import urlopen
 
-# make this WikiScraper class for scaling to scrape other wiki pages
-
 BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
 
-def make_soup(url):
-    html = urlopen(url.read())
+def make_soup(BASE_URL):
+    html = urlopen(BASE_URL).read()
     return BeautifulSoup(html, "lxml")
 
-def get_body_content(section_url):
-    html = urlopen(section_url).read()
-    soup = make_soup(section_url)
+def get_body_content(BASE_URL):
+    soup = make_soup(BASE_URL)
     body_content = soup.find("div", {"id": "bodyContent"})
-    #category_links = [BASE_URL + ]
+    return body_content
+
+get_body_content(BASE_URL)
