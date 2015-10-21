b8888621162ae2251b72f4274f1aae73d8c90c5a
MatthewKrey <kreyenglish@gmail.com>
2015-10-21 17:53:41 -0400

    add experiments with BeautifulSoup

--- wiki-word-dict.py
+++ wiki-word-dict.py
@@ -1,11 +1,14 @@
 # This solution will explore the use of:
 # BeautifulSoup & lxml libraries for HTML/XML Parsing
 # urllib for making HTTP Requests
+# bleach for HTML validation & removing unnecessary elements
 
 # make this a WikiScraper class for scaling to scrape other wiki pages
 
 from bs4 import BeautifulSoup
 from urllib import urlopen
+#import bleach   # ipynb explodes here
+
 
 BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
 
@@ -15,7 +18,10 @@ def make_soup(BASE_URL):
 
 def get_body_content(BASE_URL):
     soup = make_soup(BASE_URL)
-    body_content = soup.find("div", {"id": "bodyContent"})
-    return body_content
+    body_content = soup.find_all("div", {"id": "bodyContent"}, text='b' ) # returns collection of HTML tag objects
+    return body_content.get_text().splitlines()
+
+
+#def _bleach_html(BASE_URL)
 
 get_body_content(BASE_URL)
