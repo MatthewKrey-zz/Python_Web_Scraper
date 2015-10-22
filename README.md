a59660f2c29b69b4fd0ffaab772972fdc4f88064
MatthewKrey <kreyenglish@gmail.com>
2015-10-22 12:19:26 -0400

    add get_image_links. BOOM

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 # Experiment Goals
 1) Explore the use of BeautifulSoup & lxml libraries for HTML/XML Parsing </br>
 2) Explore the use of urllib, requests, mechanize and other libraries for making HTTP Requests. </br>
-3) Construct dictionary of page words & word counts
-4) Include basic descriptive statistics of words & word count dictionary with Pandas
+3) Construct dictionary of page words & word counts </br>
+4) Final output should, at least, convert a dictionary to a Pandas DataFrame for sorting & reporting </br>
+</br>
+# Unexpected Turns
+1) Web crawling & building basic web spider algorithm </br>
---
+++ wiki-images-no-spider.py
@@ -0,0 +1,34 @@
+# This experiment will explores the use of a page spider algorithm
+
+import urlparse
+import urllib
+import re
+from bs4 import BeautifulSoup
+from IPython import embed
+
+BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
+
+def make_soup(BASE_URL):
+    html = urllib.urlopen(BASE_URL).read()
+    return BeautifulSoup(html, "lxml")
+
+def get_image_links(BASE_URL):
+    soup = make_soup(BASE_URL)
+    anchor_tags = soup.select('a[href^="http"]')
+    hrefs = []
+    for link in anchor_tags:
+        hrefs.append(link.get("href"))
+    external_images = []
+    i = 0
+    for site in hrefs:
+        try:
+            external_soup = make_soup(site)
+            external_image = external_soup.img.get('src')
+            urllib.urlretrieve(site + external_image, "downloaded_image" + str(i) + re.findall('\.jpg$|\.jpeg$|\.png$|\.gif$', external_image)[0])
+            external_images.append(external_image)
+            i += 1
+        except(IOError, AttributeError, TypeError, IndexError):
+            continue
+    return external_images
+
+print get_image_links(BASE_URL)
