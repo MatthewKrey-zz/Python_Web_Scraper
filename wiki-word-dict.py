c9ce321b569535fcce2687051535f63e3865b0b5
MatthewKrey <kreyenglish@gmail.com>
2015-10-22 07:47:38 -0400

    add Output folder. Add wiki-word-dictionary.csv to Output folder

--- wiki-word-dict.py
+++ wiki-word-dict.py
@@ -1,9 +1,13 @@
-# This solution will explore a quick & dirty wiki page scrape
-# with the Wikipedia Python library & regular expressions
+# This solution will explore a quick & dirty Wiki page scrape
+# with the Wikipedia Python library, regular expressions & Pandas
 
 import wikipedia
 import re
 from collections import Counter
+import pandas as pd
+from pandas import DataFrame
+import csv
+
 
 class WikiScrape(object):
     def __init__(self, page_title):
@@ -19,4 +23,17 @@ class WikiScrape(object):
         content_word_list = self.get_words()
         return Counter(content_word_list)
 
-print WikiScrape("Cancer").get_word_count()
+
+# class WikiDataFrame(object):
+#     def __init__(self, dictionary):
+#         self.dictionary = WikiScrape(page_title).get_word_count
+
+def dict_to_dataframe(wiki_word_dictionary):
+    df = pd.DataFrame(wiki_word_dictionary.items(), columns=['Word', 'Word_Count'])
+    wiki_words_df = df.sort(columns="Word_Count", ascending=False)
+    return wiki_words_df
+
+
+wiki_word_dictionary = WikiScrape("Cancer").get_word_count()
+#WikiDataFrame(wiki_word_dictionary)
+print dict_to_dataframe(wiki_word_dictionary).to_csv('wiki_word_dictionary.csv', sep=':', index=False, index_label=None)
