4557096e692a86ccce4257dbdc22e10c26d7f6a7
MatthewKrey <kreyenglish@gmail.com>
2015-10-22 05:23:21 -0400

    begin simple web crawler for wiki-images

--- wiki-images.py
+++ wiki-images.py
@@ -4,7 +4,20 @@
 
 import urlparse
 import urllib
-import wikipedia
 from bs4 import BeautifulSoup
 
 url = "https://en.wikipedia.org/wiki/Cancer"
+
+urls = [url] # stack of urls to scrape
+visited = [url] # "historic" record of urls
+
+while len(urls) > 0:
+    try:
+        htmltext = urllib.urlopen(urls[0].read())
+    except:
+        print urls[0]
+    soup = BeautifulSoup(url)
+
+    urls.pop(0)
+
+    print soup.findAll('a', href=True)
