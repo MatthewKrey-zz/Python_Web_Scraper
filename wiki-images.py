d95bf80beed6b17d56e17c8d54b39741f9a3267a
MatthewKrey <kreyenglish@gmail.com>
2015-10-22 06:10:54 -0400

    add page spider algo with url stack. remove unnecessary files

--- wiki-images.py
+++ wiki-images.py
@@ -1,6 +1,4 @@
-# This experiment will explore the use of:
-# BeautifulSoup & lxml libraries for HTML/XML Parsing
-# urllib for making HTTP Requests
+# This experiment will explores the use of a page spider algorithm
 
 import urlparse
 import urllib
@@ -11,13 +9,24 @@ url = "https://en.wikipedia.org/wiki/Cancer"
 urls = [url] # stack of urls to scrape
 visited = [url] # "historic" record of urls
 
+def make_soup(BASE_URL):
+    html = urllib.urlopen(BASE_URL).read()
+    return BeautifulSoup(html, "lxml")
+
 while len(urls) > 0:
     try:
         htmltext = urllib.urlopen(urls[0].read())
     except:
         print urls[0]
-    soup = BeautifulSoup(url)
+    soup = make_soup(url)
 
     urls.pop(0)
+    print len(urls)
 
-    print soup.findAll('a', href=True)
+    for tag in soup.findAll('a', href=True):
+        tag['href'] = urlparse.urljoin(url, tag['href'])
+        if url in tag['href'] and tag['href'] not in visited:
+            urls.append(tag['href'])
+            visited.append(tag['href'])
+
+print visited
