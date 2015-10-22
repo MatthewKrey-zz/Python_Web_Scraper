2fd683b9265311fbcaa1d5e6e877417015fb58a0
MatthewKrey <kreyenglish@gmail.com>
2015-10-22 05:05:13 -0400

    add changes wiki-images

--- wiki-images-new.py
+++ wiki-images-new.py
@@ -29,51 +29,24 @@ cj = CookieJar() # in case site requires storage of cookies
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 opener.addheaders = [('User-agent'), 'Mozilla/5.0'] # do not visit as robot
 
-for reference in wiki_references:
+def main():
+    try:
+        for reference in wiki_references:
+            page = reference
+            sourceCode = opener.open(page).read()
+            print sourceCode
 
-    # a place to store the links we find
-    links = []
+    #     try:
+    #         titles = re.findall(r'',sourceCode)
+    #         links = re.findall(r'(.*?)',sourceCode)
+    #
+    #         for link in links:
+    #             print link
+    #     except Exception, e:
+    #         print str(e)
+    #
+    except Exception,e:
+        print str(e)
+        pass
 
-    r = requests.get(reference)
-    soup = BeautifulSoup(page)
-    for link in soup.findAll('a', href=True):
-        # skip useless links
-        if link['href'] == '' or link['href'].startswith('#'):
-            continue
-        # initialize the link
-        thisLink = {
-            'url': link['href'],
-            'title': link.string,
-            'image': '',
-        }
-        # see if the link contains an image
-        img = link.find('img', src=True)
-        if img:
-            thisLink['image'] = img['src']
-            if thisLink['title'] is None:
-                # look for a title here if none exists
-                if 'title' in img:
-                    thisLink['title'] = img['title']
-                elif 'alt' in img:
-                    thisLink['title'] = img['alt']
-                else:
-                    thisLink['title'] = path.basename(img['src'])
-
-        if thisLink['title'] is None:
-            # check for text inside the link
-            if len(link.contents):
-                thisLink['title'] = ' '.join(link.stripped_strings)
-        if thisLink['title'] is None:
-            # if there's *still* no title (empty tag), skip it
-            continue
-        # convert to something immutable for storage
-        hashableLink = (thisLink['url'].strip(),
-                        thisLink['title'].strip(),
-                        thisLink['image'].strip())
-        # store the result
-        if hashableLink not in links:
-            links.append(hashableLink)
-
-    # print the results
-    for link in links:
-        stdout.write('\t'.join(link) + '\n')
+main()
--- wiki-images.py
+++ wiki-images.py
@@ -2,39 +2,9 @@
 # BeautifulSoup & lxml libraries for HTML/XML Parsing
 # urllib for making HTTP Requests
 
+import urlparse
+import urllib
+import wikipedia
 from bs4 import BeautifulSoup
-import requests
-import urllib2
-from urllib2 import urlopen
-import re
-import cookielib, urllib2
-from cookielib import CookieJar
-import datetime
 
-BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
-
-def make_soup(BASE_URL):
-    html = urlopen(BASE_URL).read()
-    return BeautifulSoup(html, "lxml")
-
-def get_notes_images(BASE_URL):
-    soup = make_soup(BASE_URL)
-    references = soup.findAll("ol", "references")
-    return references
-
-def main():
-    try:
-        page = get_notes_images(BASE_URL)
-
-        try:
-            links = re.findall(r'<a.*?href="(.*?)"', s, page)
-            for link in links:
-                print link
-
-        except Exception, e:
-            print str(e)
-
-    except Exception, e:
-        print str(e)
-
-main()
+url = "https://en.wikipedia.org/wiki/Cancer"
