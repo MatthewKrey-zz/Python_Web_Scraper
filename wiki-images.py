927e6bfbfe9d445ce4ac9bbf3d9068541c611c14
MatthewKrey <kreyenglish@gmail.com>
2015-10-22 01:40:39 -0400

    add experimental partial solution

--- wiki-images.py
+++ wiki-images.py
@@ -4,21 +4,33 @@
 
 from bs4 import BeautifulSoup
 from urllib import urlopen
+import wikipedia
+import re
+# <div class="reflist columns references-column-count references-column-count-2">
+#
+# class WikiScrape(object):
+#     def __init__(self, page_title):
+#         self.content = wikipedia.page(page_title).content
+#
+#     def get_content(self):
+#         return self.content
+#
+#     def get_words(self):
+#         return re.findall("[a-zA-Z]+", self.content)
+#
+# print WikiScrape("Cancer").get_word_count()
 
-# make this WikiImageScraper class for scaling to scrape other wiki pages
 
-BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
-
-def make_soup(url):
-    html = urlopen(url.read())
-    return BeautifulSoup(html, "lxml")
-
-def get_notes_images(section_url):
-    html = urlopen(section_url).read()
-    soup = make_soup(section_url)
-    notes_images = soup.find("div", {"id": "bodyContent"}) # need to identify anchor tags for each external text
-    # pseudo code placeholder for assigning notes_image_links
-    # notes_image_links = [ BASE_URL + {li.a["rel"], class="external text", a.["href"]} ]
-    return notes_image_links
-
-# get_notes_images(section_url)
+# BASE_URL = "https://en.wikipedia.org/wiki/Cancer"
+#
+# def make_soup(url):
+#     html = urlopen(url.read())
+#     return BeautifulSoup(html, "lxml")
+#
+# def get_notes_images(section_url):
+#     html = urlopen(section_url).read()
+#     soup = make_soup(section_url)
+#     notes_images = soup.find # need to identify anchor tags for each external text
+#     # pseudo code placeholder for assigning notes_image_links
+#     # notes_image_links = [ BASE_URL + {li.a["rel"], class="external text", a.["href"]} ]
+#     return notes_image_links
